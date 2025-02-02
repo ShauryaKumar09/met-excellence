70eab8744580445ded4113f43fdbeeebea6f1701
MohanCodes <mohanatkuri@gmail.com>
2025-02-01 20:52:22 -0600

    image import get rid

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import Navbar from "@/components/Navbar";
 import Face from "@/components/Face";
-import Image from "next/image";
 import OurMissionPage from "@/components/OurMission";
 import TutorSection from "@/components/TutorSection";
 import Footer from "@/components/Footer";
